b6797c7f47375d6115a14dbfefddbb06920a697c
Arnab Nath <arnabnath123456789@gmail.com>
2023-08-09 03:04:20 +0530

    team page complete

--- app/team/former/page.tsx
+++ app/team/former/page.tsx
@@ -2,98 +2,389 @@
 import ProfileCard from "../components/ProfileCard";
 
 const PresentMembers = () => {
-  const members = [
+
+  //former members details
+  const formerMembers=[
     {
-      firstname: 'Arnab',
-      lastname: 'Ghosh',
-      role: 'President',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '4th Year, B.Tech, CH',
+      firstname: 'Chandrima',
+      lastname: 'Roy Choudhury',
+      role: 'Former President',
+      imageUrl: '/teams/former/Chandrima.jpg',
+      details: 'Class Of 2023, B.Tech, EC',
     },
     {
-      firstname: 'John',
-      lastname: 'Doe',
-      role: 'Vice President',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '3rd Year, B.Tech, CS',
+      firstname: 'Meenakshi ',
+      lastname: ' Harikumar',
+      role: 'Former Vice President',
+      imageUrl: '/teams/former/Meenakshi.jpg',
+      details: 'Class Of 2023, B.Tech, CE',
     },
     {
-      firstname: 'Jane',
-      lastname: 'Smith',
-      role: 'Secretary',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '2nd Year, B.Tech, EE',
+      firstname: 'Sayan',
+      lastname:'Majumder',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/C.jpg',
+      details: 'Class Of 2023, B.Tech, MME',
     },
     {
-      firstname: 'Alice',
-      lastname: 'Johnson',
-      role: 'Treasurer',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
+      firstname: 'Ahammad Imtiaj ',
+      lastname:'Ali',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/Imtiaj.jpg',
+      details: 'Class Of 2023, B.Tech, MME',
+    },
+    {
+      firstname: 'Sukrity ',
+      lastname:'Ghosh',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/sukrity.jpg',
+      details: 'Class Of 2023, B.Tech, ME',
+    },
+    {
+      firstname: 'Atipamula venkata sai',
+      lastname:'karthik',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/Atipamula venkata sai karthik _Civil.jpg',
+      details: 'Class Of 2023, B.Tech, CE',
+    },
+    {
+      firstname: 'Durga Shankar Pati',
+      lastname:'Tripathi',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/Durga Shankar Pati Tripathi_ Mechanical Engineering .jpg',
+      details: 'Class Of 2023, B.Tech, ME',
+    },
+    {
+      firstname: 'Kumbla Anupa',
+      lastname:'Nayak',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/Kumbla_Anupa_Nayak.jpg',
+      details: 'Class Of 2023, MTech, CE',
+    },
+    {
+      firstname: 'Sonali',
+      lastname:'Sah',
+      role:'Former Treasurer',
+      imageUrl: '/teams/former/Sonali.jpg',
+      details: 'Class Of 2023, B.Tech, CH',
+    },
+    {
+      firstname: 'Souvik',
+      lastname:'Mukherjee',
+      role:'Former Joint Treasurer',
+      imageUrl: '/teams/former/Souvik.jpeg',
+      details: 'Class Of 2023, B.Tech, ME',
+    },
+    {
+      firstname: 'Anwer',
+      lastname:'Sayeed',
+      role:'Former Web Development',
+      imageUrl: '/teams/former/anwer sayeed.JPG',
+      details: 'Class Of 2023, B.Tech, CS',
+    },
+    {
+      firstname: 'Deboleena',
+      lastname:'Das',
+      role:'Former Secretary (EPM)',
+      imageUrl: '/teams/former/Deboleena Das_Electrical.jpg',
+      details: 'Class Of 2023, B.Tech, EE',
+    },
+    {
+      firstname: 'Lisha',
+      lastname:'Saha',
+      role:'Former Joint Secretary (EPM)',
+      imageUrl: '/teams/former/Lisa.jpeg',
+      details: 'Class Of 2023, B.Tech, ME',
+    },
+    {
+      firstname: 'Ritabrata',
+      lastname:'Ganguly',
+      role:'Former Joint Secretary (EPM)',
+      imageUrl: '/teams/former/ritabrata.jpg',
+      details: 'Class Of 2023, B.Tech, CH',
+    },
+    {
+      firstname: 'Sohini',
+      lastname:'Bhattacharya',
+      role:'Former Secretary (PWG)',
+      imageUrl: '/teams/former/Sohini.jpg',
+      details: 'Class Of 2023, B.Tech, EE',
+    },
+    {
+      firstname: 'Tuhina',
+      lastname:'Rakshit',
+      role:'Former Joint Secretary (EPM)',
+      imageUrl: '/teams/former/Tuhina.jpg',
+      details: 'Class Of 2023, B.Tech, ME',
+    },
+    {
+      firstname: 'Dipayan',
+      lastname:'Dalal',
+      role:'Former President',
+      imageUrl: '/teams/former/DipayanDalal.jpg',
+      details: 'Class Of 2022, B.Tech, ME',
+    },
+    {
+      firstname: 'Bedashruti',
+      lastname:'Majumdar',
+      role:'Former Vice President ',
+      imageUrl: '/teams/former/BedashrutiMajumdar.jpg',
+      details: 'Class Of 2022, B.Tech, BT',
+    },
+    {
+      firstname: 'Adrija',
+      lastname:'Biswas',
+      role:'Former  Secretary (PWG)',
+      imageUrl: '/teams/former/AdrijaBiswas.jpg',
+      details: 'Class Of 2022, B.Tech, ME',
+    },
+    {
+      firstname: 'Ananya',
+      lastname:'Nath',
+      role:'Former  Secretary (EPM)',
+      imageUrl: '/teams/former/AnanyaNath.jpg',
+      details: 'Class Of 2022, B.Tech, ME',
+    },
+    {
+      firstname: 'Ankita',
+      lastname:'Singh',
+      role:'Former Joint Secretary (EPM)',
+      imageUrl: '/teams/former/AnkitaSingha.jpg',
+      details: 'Class Of 2022, B.Tech, ME',
+    },
+    {
+      firstname: 'Ambika Biswas',
+      lastname:'Neela',
+      role:'Former Treasurer',
+      imageUrl: '/teams/former/AmbikaBiswasNeela.jpeg',
+      details: 'Class Of 2022, B.Tech, EE',
+    },
+    {
+      firstname: 'Anushka',
+      lastname:'Sen',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/AnushkaSen.jpg',
+      details: 'Class Of 2022, B.Tech, ME',
+    },
+    {
+      firstname: 'Satti',
+      lastname:'Jyothi',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/SattiJyothi.jpg',
+      details: 'Class Of 2022, B.Tech, EC',
+    },
+    {
+      firstname: 'Ankita',
+      lastname:'Mishra',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/AnkitaMishra.jpg',
+      details: 'Class Of 2022, B.Tech, ME',
+    },
+    {
+      firstname: 'Girishma',
+      lastname:'Terli',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/GirishmaTerli.jpg',
+      details: 'Class Of 2022, B.Tech, ME',
+    },
+    {
+      firstname: 'Chavan',
+      lastname:'Swathi',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/ChavanSwathi.jpg',
+      details: 'Class Of 2022, B.Tech, BT',
+    },
+    {
+      firstname: 'Khyathirmaye ',
+      lastname:'Vanumu',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/KhyatiVanumu.jpg',
+      details: 'Class Of 2022, B.Tech, ME',
+    },
+    {
+      firstname: 'Rabin',
+      lastname:'Mondal',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/rabin.jpeg',
+      details: 'Class Of 2022, B.Tech, MME',
+    },
+    {
+      firstname: 'Dhrubajyoti',
+      lastname:'Kumar',
+      role:'Former Senior Executive Member',
+      imageUrl: '/teams/former/DhrubajyotiGupta.jpg',
+      details: 'Class Of 2022, B.Tech, ME',
+    },
+    {
+      firstname: 'Suborno',
+      lastname:'Gupta',
+      role:'Former President',
+      imageUrl: '/teams/former/suborno.jpg',
+      details: 'Class Of 2021, B.Tech, ME',
+    },
+    {
+      firstname: 'Shounak',
+      lastname:'Dasgupta',
+      role:'Former Secretary',
+      imageUrl: '/teams/former/shounak.jpg',
+      details: 'Class Of 2021, B.Tech, ME',
+    },
+    {
+      firstname: 'Sushobhan',
+      lastname:'Sarkar',
+      role:'Former Secretary',
+      imageUrl: '/teams/former/sushobhan.jpg',
+      details: 'Class Of 2021, B.Tech, ME',
+    },
+    {
+      firstname: 'Aitihya',
+      lastname:'Karmakar',
+      role:'Former Treasurer',
+      imageUrl: '/teams/former/aitihya.jpg',
+      details: 'Class Of 2021, B.Tech, EE',
+    },
+    {
+      firstname: 'Sudeepta',
+      lastname:'Gorai',
+      role:'Former Executive Member',
+      imageUrl: '/teams/former/sudeepta.jpg',
+      details: 'Class Of 2021, B.Tech, ME',
+    },  
+    {
+      firstname: 'Anisha',
+      lastname:'Majumdar',
+      role:'Former Executive Member',
+      imageUrl: '/teams/former/anisha.jpg',
+      details: 'Class Of 2021, B.Tech, ME',
+    },  
+    {
+      firstname: 'Dhrubajyoti',
+      lastname:'Gupta',
+      role:'Former Executive Member',
+      imageUrl: '/teams/former/DhrubajyotiGupta.jpg',
+      details: 'Class Of 2021, B.Tech, ME',
+    },  
+    {
+      firstname: 'Sagnik',
+      lastname:'Charkraborty',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/sagnik.jpg',
+      details: 'Class Of 2020, B.Tech, ME',
+    },  
+    {
+      firstname: 'Sibam',
+      lastname:'Roy',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/sibam.jpg',
+      details: 'Class Of 2020, B.Tech, ME',
+    },  
+    {
+      firstname: 'Tiasa',
+      lastname:'Das',
+      role:'Former President',
+      imageUrl: '/teams/former/Tiasa.jpg',
+      details: 'Class Of 2020, B.Tech, ME',
+    },  
+    {
+      firstname: 'Abishek',
+      lastname:'Gupta',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/abhisek.jpg',
+      details: 'Class Of 2020, B.Tech, ME',
+    },  
+    {
+      firstname: 'Sukanta',
+      lastname:'Manna',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/sukanta.jpg',
+      details: 'Class Of 2020, B.Tech, EE',
+    },  
+    {
+      firstname: 'Debangshu',
+      lastname:'Pramanik',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/debangshu.jpg',
+      details: 'Class Of 2020, B.Tech, ME',
+    },  
+    {
+      firstname: 'Gaurish Kumar',
+      lastname:'Shaw',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/gaurish.jpg',
+      details: 'Class Of 2020, B.Tech, ME',
+    },  
+    {
+      firstname: 'Pallinti Krishna',
+      lastname:' Chaitanya',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/pallinti_Krishna_chaitanya.jpg',
+      details: 'Class Of 2020, B.Tech, ME',
+    },  
+    {
+      firstname: 'Meena kumari',
+      lastname:' Jana',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/Meena.jpg',
+      details: 'Class Of 2020, B.Tech, MME',
+    }, 
+    {
+      firstname: 'Nikhil Kumar',
+      lastname:'Kandula',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/Nikhil.jpg',
+      details: 'Class Of 2020, B.Tech, ME',
+    },  
+    {
+      firstname: 'Amit Kumar',
+      lastname:'Ball',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/amit.jpg',
+      details: 'PhD Scholar, CS',
+    },  
+    {
+      firstname: 'Chayan',
+      lastname:'Ghosh',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/user.jpg',
       details: 'Final Year, B.Tech, ME',
-    },
+    },  
     {
-      firstname: 'Michael',
-      lastname: 'Brown',
-      role: 'Head of Marketing',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '4th Year, B.Tech, ECE',
-    },
+      firstname: 'Sk.',
+      lastname:'Mehabub',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/user.jpg',
+      details: 'Final Year, B.Tech, ME',
+    },  
     {
-      firstname: 'Emily',
-      lastname: 'Lee',
-      role: 'Events Coordinator',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '3rd Year, B.Tech, CE',
+      firstname: 'Devansh.',
+      lastname:'Goenka',
+      role:'Former SAIC Member',
+      imageUrl: '/teams/former/user.jpg',
+      details: 'Final Year, B.Tech, CSE',
     },
-    {
-      firstname: 'David',
-      lastname: 'Miller',
-      role: 'Public Relations Officer',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '2nd Year, B.Tech, AE',
-    },
-    {
-      firstname: 'Sophia',
-      lastname: 'Wang',
-      role: 'Technical Lead',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: 'Final Year, B.Tech, IT',
-    },
-    {
-      firstname: 'Daniel',
-      lastname: 'Wilson',
-      role: 'Media Coordinator',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '4th Year, B.Tech, CSE',
-    },
-    {
-      firstname: 'Olivia',
-      lastname: 'Taylor',
-      role: 'Creative Director',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '3rd Year, B.Tech, ME',
-    },
-  ]
+]
 
 return (
-    <div className="flex flex-col py-16 px-30" style={{ paddingTop: '30px' }}>
-      <h3 className="text-center text-orange-600 text-4xl">Former SAI Cell member</h3>
+  <div className="flex flex-col py-16 px-30" style={{ paddingTop: '30px' }}>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 my-7 px-3 md:mx-[30px] py-6  pl-40" style={{ display: 'grid', justifyContent: 'center', alignContent: 'center' }}>
-        {members.map((member, index) => (
-          <div key={index} className="mb-8 items-center justify-between mx-auto" style={{ justifySelf: 'center', alignSelf: 'center' }}>
-            <ProfileCard
-              firstname={member.firstname}
-              lastname={member.lastname}
-              role={member.role}
-              imageUrl={member.imageUrl}
-              details={member.details}
-            />
-          </div>
-        ))}
+  {/* official bearers */}
+  <h3 className="text-center text-orange-600 text-4xl">Office Bearers (2023-2024)</h3>
+
+  <div className="grid grid-cols-2 md:grid-cols-4 my-7 px-3 md:mx-[30px] py-6  pl-40" style={{ display: 'grid', justifyContent: 'center', alignContent: 'center' }}>
+    {formerMembers.map((member, index) => (
+      <div key={index} className="mb-8 items-center justify-between mx-auto" style={{ justifySelf: 'center', alignSelf: 'center' }}>
+        <ProfileCard
+          firstname={member.firstname}
+          lastname={member.lastname}
+          role={member.role}
+          imageUrl={member.imageUrl}
+          details={member.details}
+        />
       </div>
+    ))}
+  </div>
 
-    </div>
+  </div>
   );
 };
 
--- app/team/present/page.tsx
+++ app/team/present/page.tsx
@@ -2,7 +2,9 @@
 import ProfileCard from "../components/ProfileCard";
 
 const PresentMembers = () => {
-  const officialMembers = [
+
+  // official members details
+  const officialBearer = [
     {
       firstname: 'Arnab',
       lastname: 'Ghosh',
@@ -11,87 +13,326 @@ const PresentMembers = () => {
       details: '4th Year, B.Tech, CH',
     },
     {
-      firstname: 'John',
-      lastname: 'Doe',
-      role: 'Vice President',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '3rd Year, B.Tech, CS',
+      firstname: 'Agnila',
+      lastname: 'Saha',
+      role: 'Vice-President, Content Writing Head',
+      imageUrl: '/teams/present/agnila_saha.jpg',
+      details: '4th Year, B.Tech, BT',
     },
     {
-      firstname: 'Jane',
-      lastname: 'Smith',
-      role: 'Secretary',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '2nd Year, B.Tech, EE',
+      firstname: 'Md Nasir',
+      lastname: '',
+      role: 'General Secretary',
+      imageUrl: '/teams/present/Md Nasir-Civil Engineering-1st year.jpg',
+      details: '4th Year, B.Tech, CE',
     },
 
     {
-      firstname: 'David',
-      lastname: 'Miller',
-      role: 'Public Relations Officer',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '2nd Year, B.Tech, AE',
+      firstname: 'Debjit',
+      lastname: 'Maji',
+      role: 'Joint Treasurer',
+      imageUrl: '/teams/present/debjit_maji.jpg',
+      details: '4th Year, B.Tech,  EE',
     },
-  ]
+    {
+      firstname: 'Deepak',
+      lastname: 'Varalasetty',
+      role: 'Graphics and Videography Head,IT Head',
+      imageUrl: '/teams/present/Deepak varalasetty_ECE.png',
+      details: '4th Year, B.Tech,CS',
+    },
+    {
+      firstname: 'Deepika',
+      lastname: 'chenna',
+      role: 'Event and Publicity Head',
+      imageUrl: '/teams/present/Deepika_chenna.jpg',
+      details: '4th Year, B.Tech, ME',
+    },
+    {
+      firstname: 'Souvik',
+      lastname: 'Pal',
+      role: 'General Secretary',
+      imageUrl: '/teams/present/Souvik Pal_BT.jpg',
+      details: '4th Year, B.Tech, BT',
+    },
 
+
+  ];
+
+  //executive members details
 
   const executiveMembers = [
     {
-      firstname: 'Sophia',
-      lastname: 'Wang',
-      role: 'Technical Lead',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: 'Final Year, B.Tech, IT',
+      firstname: 'Abheerup',
+      lastname: 'Sarkar',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/abhirup_s (1).jpg',
+      details: '4th Year, B.Tech, BT',
     },
     {
-      firstname: 'Daniel',
-      lastname: 'Wilson',
-      role: 'Media Coordinator',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '4th Year, B.Tech, CSE',
+      firstname: 'Aniket ',
+      lastname: 'Kamble',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/Aniket_Kamble.jpg',
+      details: '3rd Year, B.Tech, CH',
     },
     {
-      firstname: 'Olivia',
-      lastname: 'Taylor',
-      role: 'Creative Director',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
+      firstname: 'Arindam ',
+      lastname: 'Chatterjee',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/arindan_c.jpg',
+      details: '3rd Year, B.Tech, MME',
+    },
+    {
+      firstname: 'Balli ',
+      lastname: 'Chanukya',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/balli_c.jpg',
+      details: '3rd Year, B.Tech, CSE',
+    },
+    {
+      firstname: 'Nikhil',
+      lastname: 'Kumar',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/nikhil_k.jpg',
       details: '3rd Year, B.Tech, ME',
+    },
+    {
+      firstname: 'N.Surya',
+      lastname: 'Hemanth',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/nsh.jpg',
+      details: '3rd Year, B.Tech, ECE',
+    },
+    {
+
+      firstname: 'Rishav ',
+      lastname: 'Mondal',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/Rishav.jpg',
+      details: '3rd Year, B.Tech, ME',
+    },
+    {
+      firstname: 'Samanvita',
+      lastname: '',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/samavita.jpg',
+      details: '3rd Year, B.Tech, ECE',
+    },
+    {
+      firstname: 'Shayoni ',
+      lastname: 'Mahato',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/shayoni_m.jpg',
+      details: '3rd Year, B.Tech, EE',
+    },
+    {
+      firstname: 'SHUBHAM',
+      lastname: 'GHOSH',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/SHUBHAM_GHOSH.jpg',
+      details: '3rd Year, B.Tech, MME',
+    },
+    {
+      firstname: 'Sneha ',
+      lastname: 'Ghosh',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/sneha_g.jpg',
+      details: '3rd Year, B.Tech, IC',
+    },
+    {
+      firstname: 'Soumik ',
+      lastname: 'Roy Choudhury',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/soumak_rc.jpg',
+      details: '3rd Year, B.Tech, CSE',
+    },
+    {
+      firstname: 'Agnila',
+      lastname: 'Saha',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/Deepika_chenna.jpg',
+      details: '3rd Year, B.Tech, CH',
+    },
+    {
+      firstname: 'SOUMYADIP',
+      lastname: 'DEBNATH',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/SOUMYADIP_DEBNATH.jpg',
+      details: '3rd Year, B.Tech, ME',
+    },
+    {
+      firstname: 'T.V.N.S.PRAVEEN',
+      lastname: '',
+      role: 'SAIC Executive Member',
+      imageUrl: '/teams/present/T.V.N.S.PRAVEEN.jpg',
+      details: '3rd Year, B.Tech, ECE',
     },
   ]
 
+  // cell members pics
+
   const cellMembers = [
     {
-      firstname: 'Alice',
-      lastname: 'Johnson',
-      role: 'Treasurer',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: 'Final Year, B.Tech, ME',
+      firstname: 'Satyak ',
+      lastname: 'Chaudhuri',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/2.jpg',
+      details: '4th Year, B.Tech, ME',
     },
     {
-      firstname: 'Michael',
-      lastname: 'Brown',
-      role: 'Head of Marketing',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
+      firstname: 'Amanjyoti',
+      lastname: 'Mullick',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Amanjyoti_Mullick_3rd_EE.jpg',
+      details: '4th Year, B.Tech, EE',
+    },
+    {
+      firstname: 'Arnab',
+      lastname: 'Nath',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Arnab_Nath_3rd_CSE.jpg',
+      details: '4th Year, B.Tech, CSE',
+    },
+    {
+      firstname: 'Bala',
+      lastname: 'Ganesh',
+      role: ' SAIC Member',
+      imageUrl: '/teams/present/Bala_Ganesh_1st_CSE.jpg',
+      details: '4th Year, B.Tech, CSE',
+    },
+
+    {
+      firstname: 'Katravath',
+      lastname: 'Navya',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Katravath_Navya_1st_ME.jpg',
+      details: '4th Year, B.Tech, ME',
+    },
+    {
+      firstname: 'Pappu ',
+      lastname: 'Sharmila',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Pappu_Sharmila_1st_ECE.jpg',
       details: '4th Year, B.Tech, ECE',
     },
     {
-      firstname: 'Emily',
-      lastname: 'Lee',
-      role: 'Events Coordinator',
-      imageUrl: '/teams/present/Arnab_ghosh.jpg',
-      details: '3rd Year, B.Tech, CE',
+      firstname: 'Pratishtha',
+      lastname: 'Barua',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Pratishtha_Barua_1st_EE.jpg',
+      details: '4th Year, B.Tech, EE',
     },
+    {
+      firstname: 'Rahul ',
+      lastname: 'Mondal',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Rahul_Mondal_1st_ME.jpg',
+      details: '4th Year, B.Tech, ME',
+    },
+    {
+      firstname: 'Reshmi',
+      lastname: 'Saha',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Reshmi_Saha_1st_EC.jpg',
+      details: '4th Year, B.Tech, EC',
+    },
+    {
+      firstname: 'Sai',
+      lastname: 'Kiran',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Sai_Kiran_3rd_EE.jpg',
+      details: '4th Year, B.Tech, EE',
+    },
+    {
+      firstname: 'Sai_Kumar',
+      lastname: '',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Sai_Kumar_1st_ME.jpg',
+      details: '4th Year, B.Tech, ME',
+    },
+    {
+      firstname: 'Sanjib ',
+      lastname: 'Maity',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/SANJIB_MAITY_1st_MME.jpg',
+      details: '4th Year, B.Tech, MME',
+    },
+    {
+      firstname: 'Satish ',
+      lastname: 'Abbadasari',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Satish_Abbadasari_3rd_CSE.jpg',
+      details: '4th Year, B.Tech, CSE B',
+    },
+    {
+      firstname: 'Souhardya ',
+      lastname: 'Majumder',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Souhardya_Majumder_1st_ECE.jpg',
+      details: '4th Year, B.Tech, ECE',
+    },
+    {
+      firstname: 'Soumi ',
+      lastname: 'Ghosh',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Soumi_Ghosh_1st_CH.jpg',
+      details: '4th Year, B.Tech, CH',
+    },
+    {
+      firstname: 'Soumili',
+      lastname: 'Ghosh',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Soumili_Ghosh_1st_MME.jpg',
+      details: '4th Year, B.Tech, MME',
+    },
+    {
+      firstname: 'Sudipta',
+      lastname: 'Das',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Sudipta_Das_3rd_CSE.jpg',
+      details: '4th Year, B.Tech, CSE',
+    },
+    {
+      firstname: 'Supriyo',
+      lastname: 'Mondal',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Supriyo_mondal_1st_CE (1).jpg',
+      details: '4th Year, B.Tech, CE',
+    },
+    {
+      firstname: 'Swarnim',
+      lastname: 'Suryansh',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Swarnim_MM.jpg',
+      details: '4th Year, B.Tech, MME',
+    },
+    {
+      firstname: 'Uppada ',
+      lastname: 'Sravanthi',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Uppada_Sravanthi_1st_ME.jpg',
+      details: '4th Year, B.Tech, ME',
+    },
+    {
+      firstname: 'Vishal',
+      lastname: 'Gupta',
+      role: 'SAIC Member',
+      imageUrl: '/teams/present/Vishal_Gupta_1st_ME.jpg',
+      details: '4th Year, B.Tech, ME',
+    },
+
   ]
 
 
   return (
-    <div className="flex flex-col py-16 px-30" style={{ paddingTop: '30px' }}>
+    <div className="flex flex-col py-16 px-30 " style={{ paddingTop: '30px' }}>
 
       {/* official bearers */}
       <h3 className="text-center text-orange-600 text-4xl">Office Bearers (2023-2024)</h3>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 my-7 px-3 md:mx-[30px] py-6  pl-40" style={{ display: 'grid', justifyContent: 'center', alignContent: 'center' }}>
-        {officialMembers.map((member, index) => (
+      <div className="grid grid-cols-2 md:grid-cols-3  my-7 px-3 md:mx-[30px] py-6  pl-40" style={{ display: 'grid', justifyContent: 'center', alignContent: 'center' }}>
+        {officialBearer.map((member, index) => (
           <div key={index} className="mb-8 items-center justify-between mx-auto" style={{ justifySelf: 'center', alignSelf: 'center' }}>
             <ProfileCard
               firstname={member.firstname}
@@ -108,7 +349,7 @@ const PresentMembers = () => {
 
       {/* executive bearers */}
       <h3 className="mt-5 text-center text-orange-600 text-4xl">SAIC Executive Member</h3>
-      <div className="grid grid-cols-2 md:grid-cols-3 my-7 px-3 md:mx-[30px] py-6  pl-40" style={{ display: 'grid', justifyContent: 'center', alignContent: 'center' }}>
+      <div className="grid grid-cols-2 md:grid-cols-3 my-7 px-3 md:mx-[30px] py-6 max-md:pl-20  md:pl-40" style={{ display: 'grid', justifyContent: 'center', alignContent: 'center' }}>
         {executiveMembers.map((member, index) => (
           <div key={index} className="mb-8 items-center justify-between mx-auto" style={{ justifySelf: 'center', alignSelf: 'center' }}>
             <ProfileCard
